8ed8781d3bd7a87781758df1c1b5b65ae005daef
Girish Ramakrishnan <girish@cloudron.io>
2022-01-07 18:24:38 -0800

    asyncify tests

--- test/test.js
+++ test/test.js
@@ -11,121 +11,98 @@
 
 require('chromedriver');
 
-var execSync = require('child_process').execSync,
+const execSync = require('child_process').execSync,
     expect = require('expect.js'),
     path = require('path'),
-    superagent = require('superagent');
-
-var chrome = require('selenium-webdriver/chrome'),
-    Builder = require('selenium-webdriver').Builder;
+    superagent = require('superagent'),
+    { Builder } = require('selenium-webdriver'),
+    { Options } = require('selenium-webdriver/chrome');
 
 describe('Application life cycle test', function () {
     this.timeout(0);
 
-    let browser;
-    var LOCATION = 'test';
-    var app;
+    const LOCATION = 'test';
+    const BASIC_AUTH_USER = 'admin';
+    const BASIC_AUTH_PASS = 'changeme123';
+    const EXEC_ARGS = { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' };
 
-    let BASIC_AUTH_USER = "admin";
-    let BASIC_AUTH_PASS = "changeme123";
+    let browser, app;
 
-    let options = new chrome.Options();
-    options.windowSize({ width: 1280, height: 1024 });
-
-    before(function (done) {
-        if (!process.env.USERNAME) return done(new Error('USERNAME env var not set'));
-        if (!process.env.PASSWORD) return done(new Error('PASSWORD env var not set'));
-
-        browser = new Builder().forBrowser('chrome').setChromeOptions(options).build();
-
-        done();
+    before(function () {
+        browser = new Builder().forBrowser('chrome').setChromeOptions(new Options().windowSize({ width: 1280, height: 1024 })).build();
     });
 
-    after(function (done) {
+    after(function () {
         browser.quit();
-        done();
     });
 
-    function checkTwitterBridge(done) {
-        superagent.get(`https://${app.fqdn}/?action=display&bridge=Twitter&context=By+username&u=jack&format=Json`)
-            .auth(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-            .end(function (error, result) {
-                expect(error).to.be(null);
-                expect(result.status).to.eql(200);
-                expect(result.body.home_page_url).to.eql("https://twitter.com/jack");
+    async function checkTwitterBridge() {
+        const response = await superagent.get(`https://${app.fqdn}/?action=display&bridge=Twitter&context=By+username&u=jack&format=Json`)
+            .auth(BASIC_AUTH_USER, BASIC_AUTH_PASS);
+        expect(response.status).to.eql(200);
+        expect(response.body.home_page_url).to.eql('https://twitter.com/jack');
+    }
 
-                done();
-        });
+    function getAppInfo() {
+        const inspect = JSON.parse(execSync('cloudron inspect'));
+        app = inspect.apps.filter(function (a) { return a.location.startsWith(LOCATION); })[0];
+        expect(app).to.be.an('object');
     }
 
     xit('build app', function () {
-        execSync('cloudron build', { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron build', EXEC_ARGS);
     });
 
     it('install app', function () {
-        execSync('cloudron install --location ' + LOCATION, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron install --location ' + LOCATION, EXEC_ARGS);
     });
 
-    it('can get app information', function () {
-        var inspect = JSON.parse(execSync('cloudron inspect'));
+    it('can get app information', getAppInfo);
 
-        app = inspect.apps.filter(function (a) { return a.location === LOCATION; })[0];
-
-        expect(app).to.be.an('object');
-    });
-
-    it('can get the main page', function (done) {
-        superagent.get('https://' + app.fqdn).auth(BASIC_AUTH_USER, BASIC_AUTH_PASS).end(function (error, result) {
-            expect(error).to.be(null);
-            expect(result.status).to.eql(200);
-
-            done();
-        });
+    it('can get the main page', async function () {
+        const response = await superagent.get('https://' + app.fqdn).auth(BASIC_AUTH_USER, BASIC_AUTH_PASS);
+        expect(response.status).to.eql(200);
     });
 
     it('can use the Twitter bridge', checkTwitterBridge);
 
-    it('can restart app', function (done) {
+    it('can restart app', function () {
         execSync('cloudron restart --app ' + app.id);
-        done();
     });
 
     it('backup app', function () {
-        execSync('cloudron backup create --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron backup create --app ' + app.id, EXEC_ARGS);
     });
 
     it('restore app', function () {
         const backups = JSON.parse(execSync(`cloudron backup list --app ${app.id} --raw`));
-        execSync('cloudron uninstall --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
-        execSync('cloudron install --location ' + LOCATION, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
-        var inspect = JSON.parse(execSync('cloudron inspect'));
-        app = inspect.apps.filter(function (a) { return a.location === LOCATION; })[0];
-        execSync(`cloudron restore --backup ${backups[0].id} --app ${app.id}`, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron uninstall --app ' + app.id, EXEC_ARGS);
+        execSync('cloudron install --location ' + LOCATION, EXEC_ARGS);
+        getAppInfo();
+        execSync(`cloudron restore --backup ${backups[0].id} --app ${app.id}`, EXEC_ARGS);
     });
 
     it('can use the Twitter bridge', checkTwitterBridge);
 
     it('uninstall app', function () {
-        execSync('cloudron uninstall --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron uninstall --app ' + app.id, EXEC_ARGS);
     });
 
     // test update
     it('can install previous version from appstore', function () {
-        execSync('cloudron install --appstore-id com.rssbridgeapp.cloudronapp --location ' + LOCATION, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
-        var inspect = JSON.parse(execSync('cloudron inspect'));
-        app = inspect.apps.filter(function (a) { return a.location === LOCATION; })[0];
-        expect(app).to.be.an('object');
+        execSync('cloudron install --appstore-id com.rssbridgeapp.cloudronapp --location ' + LOCATION, EXEC_ARGS);
+        getAppInfo();
     });
 
     it('can use the Twitter bridge', checkTwitterBridge);
 
     it('can update', function () {
-        execSync('cloudron update --app ' + LOCATION, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron update --app ' + LOCATION, EXEC_ARGS);
     });
 
     it('can use the Twitter bridge', checkTwitterBridge);
 
     it('uninstall app', function () {
-        execSync('cloudron uninstall --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
+        execSync('cloudron uninstall --app ' + app.id, EXEC_ARGS);
     });
 });
